71d849cbc3ffd535e5ec820c0fa82db0518b86bc
barroso <franciscobarrosoflf@gmail.com>
2023-06-28 16:09:32 -0300

    five completed exercism

---
+++ README.md
@@ -0,0 +1,10 @@
+## solution-exercism-elixir
+
+I use this repository to solve exercises from the [Exercism](https://exercism.org/tracks/elixir/exercises/)
+
+Learn and master concepts to achieve fluency in Elixir.
+
+Journey through Elixir:
+
+<img src="exercism-journey.jpeg" width="360" height="225">
+
---
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,20 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8.0
+  end
+
+  def apply_discount(value, discount) do
+    value * (100 - discount) / 100
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    daily_rate(hourly_rate) * 22 
+    |> apply_discount(discount)
+    |> ceil
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    price = daily_rate(hourly_rate) |> apply_discount(discount)
+    budget / price |> Float.floor(1)
+  end
+end
