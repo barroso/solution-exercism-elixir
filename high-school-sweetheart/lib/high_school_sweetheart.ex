18d71d30c380aa766980eda5c0e8036f0cc9cd60
barroso <franciscobarrosoflf@gmail.com>
2023-07-13 10:29:28 -0300

    completed High School Sweetheart with Pipe Operator and String methods

--- high-school-sweetheart/lib/high_school_sweetheart.ex
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,37 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    String.trim(name) |> String.at(0)
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    first_letter(name)
+    |> String.upcase
+    |> Kernel.<>(".")
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [name, last_name] = String.split(full_name)
+    "#{initial(name)} #{initial(last_name)}"
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
-    # Please implement the pair/2 function
+    p1 = initials(full_name1)
+    p2 = initials(full_name2)
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{p1}  +  #{p2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
